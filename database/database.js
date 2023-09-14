bfa794b3c8cba3fba2d6b74fcbb8dd476d74ad05
Martin Coña <martinnewen@gmail.com>
2023-09-14 00:15:16 -0300

    Conexion a la BD, formulario de creacion de post

---
+++ database/database.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require("sequelize");
+
+
+
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbHost = process.env.DB_HOST;
+
+// conexion
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  dialect: "mysql",
+});
+
+//TEST CONEXION
+const TestConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("La conexion a la base de datos se establecio");
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+  }
+};
+
+module.exports = { sequelize, TestConnection };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 require("dotenv").config();
 const express = require("express");
-const ejs = require('ejs')
+const { sequelize, TestConnection } = require("../database/database");
+const ejs = require("ejs");
 const app = express();
 const port = process.env.PORT;
 
 //configurar EJS como motor de plantilla
 app.set("view engine", "ejs");
-app.set('views', 'views')
+app.set("views", "views");
 
-app.get("/", function (req, res) {
-  res.render("inicio", { nombre: "Martin" });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/inicio", function (req, res) {
+  res.render("inicio", {});
 });
+
+app.get("/posts/", function (req, res) {
+  res.render("posts", {});
+});
+
+app.get("/acerca_de/", function (req, res) {
+  res.render("acerca_de", {});
+});
+
+app.get("/nuevo_post/", function (req, res) {
+  res.render("nuevo_post", {});
+});
+
+app.get("/newpost/", function (req, res) {
+  res.render("newpost", {});
+});
+app.post("/newpost/", function (req, res) {
+  const { foto, descripcion } = req.body;
+  res.send("Creado!");
+});
+
+TestConnection();
 
 app.listen(port, () => {
   console.log(`El servidor esta corriendo en el puerto ${port}`);
